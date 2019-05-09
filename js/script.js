50601c3bb4d1753be5f779909185f27e54f5cc5c
Kong Cha Yang <kongchayang@gmail.com>
2019-05-09 10:41:42 -0400

    added a conditional for thenumbers to check if a result has been displayed or not.

--- js/script.js
+++ js/script.js
@@ -8,38 +8,45 @@ const input = document.getElementById('input'), // input/output button
     
 let resultDisplayed = false; // flag to keep an eye on what output is displayed
 
-// adding click handlers to number buttons
-numbers.forEach(function(number){
-    number.addEventListener('click', function(){
-        console.log(this.innerHTML);
-    })
-})
-// adding click handlers to the operation buttons
-operators.forEach(function(operator){
-    operator.addEventListener('click', function(){
-        console.log(this.innerHTML);
-    })
-})
+Array.from(numbers).map(number => {
+    number.addEventListnener("click", function() {           
+        if (resultDisplayed === false) {
+            input.innerHTML += this.innerHTML;
+        }  else if (
+            (resultDisplayed === true && lastChar === "+") ||
+            (resultDisplayed === true && lastChar === "-") ||
+            (resultDisplayed === true && lastChar === "x") ||
+            (resultDisplayed === true && lastChar === "/") 
+        ) 
+        {
+            reslutDisplayed = false;
+            input.innerTHML += this.innerHTML;
+        } else {
+            resultDisplayed =  false;
+            input.innerHTML = "";
+            input.innerHTML += this.innerHTML;
+        }
+    });
+});
 
-// on click of 'equal' button
-result.addEventListener('click', function(){
-        console.log(this.innerHTML);
+Array.from(operators).map(operator => {
+    operator.addEventListener("click", function(){
+        let currentString = input.innerHTML;
+        let lastChar = currentString[currentString.lenth - 1];
+        if (lastChar === "+" || lastChar === "-" || lastChar === "*" || lastChar === "/") {
+            const newString = currentString.substring(0, currentString.lenght - 1) + this.innerTHML;
+            input.innerHTML = newString;
+        } else if (currentString.length == 0) {
+            console,log("enter a number first");
+        } else {
+            input.innerHTML += this.innerHTML;
+        }
+    });
+});
+
+numberSeven.addEventListener('click', function(){
+    const value = numberSeven.innerHTML;
+    echoThisText(value);
 })
-// clearing the input on press of clear
-input.addEventListener('click', function(){
-    console.log(this.innerHTML);
-})
-// create a function for input to access class "number" 
-
-function inputThisText()
-
-// function inputThis(textToRepeat) {
-//     console.log(textToRepeeat);
-// }
-
-// numberSeven.addEventListener('click', function(){
-//     const value = numberSeven.innerHTML;
-//     echoThisText(value);
-// })
 
 // echo = input
