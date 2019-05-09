07af4e230ff80044f0825dab50f8c0632412c89e
kongchayang <49628328+kongchayang@users.noreply.github.com>
2019-05-09 08:20:54 -0400

    Update script.js
    My solutions

--- js/script.js
+++ js/script.js
@@ -9,9 +9,20 @@ const input = document.getElementById('input'), // input/output button
 let resultDisplayed = false; // flag to keep an eye on what output is displayed
 
 // adding click handlers to number buttons
-
+numbers.forEach(function(number){
+    number.addEventListener('click', function(){
+        console.log(this.innerHTML);
+    })
+})
 // adding click handlers to the operation buttons
-
+operators.forEach(function(operator){
+    operator.addEventListener('click', function(){
+        console.log(this.innerHTML);
+    })
+})
 // on click of 'equal' button
-
+result.addEventListener('click', function(){
+        console.log(this.innerHTML);
+})
 // clearing the input on press of clear
+function inputThisText()
