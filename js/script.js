93331706f11e9ee863be9ff9e55b824597ef2245
Kong Cha Yang <kongchayang@gmail.com>
2019-05-09 09:41:48 -0400

    some changes

--- js/script.js
+++ js/script.js
@@ -1,17 +1,45 @@
 "use strict";
 
 const input = document.getElementById('input'), // input/output button
-    number = document.querySelectorAll('.numbers div'), // number buttons
-    operator = document.querySelectorAll('.operators div'), // operator buttons
+    numbers = document.querySelectorAll('.numbers div'), // number buttons
+    operators = document.querySelectorAll('.operators div'), // operator buttons
     result = document.getElementById('result'), // equal button
     clear = document.getElementById('clear'); // clear button
     
 let resultDisplayed = false; // flag to keep an eye on what output is displayed
 
 // adding click handlers to number buttons
-
+numbers.forEach(function(number){
+    number.addEventListener('click', function(){
+        console.log(this.innerHTML);
+    })
+})
 // adding click handlers to the operation buttons
+operators.forEach(function(operator){
+    operator.addEventListener('click', function(){
+        console.log(this.innerHTML);
+    })
+})
 
 // on click of 'equal' button
-
+result.addEventListener('click', function(){
+        console.log(this.innerHTML);
+})
 // clearing the input on press of clear
+input.addEventListener('click', function(){
+    console.log(this.innerHTML);
+})
+// create a function for input to access class "number" 
+
+function inputThisText()
+
+// function inputThis(textToRepeat) {
+//     console.log(textToRepeeat);
+// }
+
+// numberSeven.addEventListener('click', function(){
+//     const value = numberSeven.innerHTML;
+//     echoThisText(value);
+// })
+
+// echo = input
